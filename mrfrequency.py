b7a0e1d17d99e79aaaf6d15d880029d7944a7802
LABINFO-FA7 - Fortes <fortes@7setlabs.local>
2019-04-02 22:25:36 -0300

    exemplo contador de palavras

---
+++ mrfrequency.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+
+class Frequency(MRJob):
+    def mapper(self, _, line):
+        words = line.split(" ")
+        for word in words:
+            yield word, 1
+
+    def reducer(self, word, values):
+        yield word, sum(values)
+
+
+if __name__ == '__main__':
+    Frequency.run()
